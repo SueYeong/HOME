aa0fb88a6cbc548aaa4b9d63fa70b4dc3d09852e
SueYeong <dudtb205@gmail.com>
2022-06-16 21:41:09 +0900

    middle

--- src/components/Stylecomponent.js
+++ src/components/Stylecomponent.js
@@ -1,19 +1,69 @@
 import styled from "styled-components";
+import { mainStyle } from "../styles/GlobalStyled";
 
-const Con_wrap = styled.div`
+const Div = styled.div`
+  width: 100wh;
+  height: 100vh;
   display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.8);
 `;
 
+const Con_wrap = styled.div`
+  width: 1400px;
+  height: 500px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Wrap = styled.div`
+  display: flex;
+  width: 400px;
+  height: 700px;
+  flex-direction: column;
+  color: ${mainStyle.color};
+`;
+
+const Box = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+`;
 export const Stylecomponent = ({ Styledata }) => {
   return (
-    <Con_wrap>
-      {Styledata.map((con) => (
-        <wrap>
-          <img src={con.img} />
-          <h3>{con.title}</h3>
-          <p>{con.explain}</p>
-        </wrap>
-      ))}
-    </Con_wrap>
+    <Div>
+      <Con_wrap>
+        {Styledata.map((con) => (
+          <Wrap>
+            <img
+              src={con.img}
+              style={{
+                width: "100%",
+                height: "200px",
+              }}
+            />
+            <Box>
+              <h3
+                style={{
+                  fontSize: "25px",
+                  marginLeft: "20px",
+                }}
+              >
+                {con.title}
+              </h3>
+              <p
+                style={{
+                  fontSize: "15px",
+                  widows: "100px",
+                  height: "100px",
+                  marginLeft: "20px",
+                }}
+              >
+                {con.explain}
+              </p>
+            </Box>
+          </Wrap>
+        ))}
+      </Con_wrap>
+    </Div>
   );
 };
--- src/styles/GlobalStyled.js
+++ src/styles/GlobalStyled.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
+export const mainStyle = {
+  color: "white",
+  fontSize: "50px",
+};
+
 export const GlobalStyled = createGlobalStyle`
     ${reset}
 
